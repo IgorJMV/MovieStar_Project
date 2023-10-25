ea7107c2c7e35dd480ed3b48b33416ceee25f2cc
IgorJMV <igorjmv2000@gmail.com>
2023-10-24 21:38:47 -0300

    Registro de usuário

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -1,40 +1,69 @@
 <?php
 
     require_once("models/User.php");
+    require_once("models/Message.php");
 
     class UserDAO implements UserDaoInterface {
         private $conn;
         private $url;
+        private $message;
 
         function __construct(PDO $conn, $url){
             $this->conn = $conn;
             $this->url = $url;
+            $this->message = new Message($url);
         }
 
         public function buildUser($data){
-            $user = new User($data["id"], $data["name"], $data["lastname"], $data["email"], $data["password"], $data["image"], $data["bio"], $data["token"]);
+            $user = new User($data["id"], $data["name"], $data["lastname"], $data["email"], $data["password"], ($data["image"] != null) ? $data["image"] : "", ($data["bio"] != null) ? $data["bio"] : "", $data["token"]);
             
             return $user;
         }
         
         public function create(User $user, $authUser = false){
-            //TOOO
+            $name = $user->getName();
+            $lastname = $user->getLastname();
+            $email = $user->getEmail();
+            $password = $user->getPassword();
+            $token = $user->getToken();
+
+            $statement = $this->conn->prepare("INSERT INTO users
+                        (name, lastname, email, password, token) VALUES
+                        (:name, :lastname, :email, :password, :token)");
+            $statement->bindParam(":name", $name);
+            $statement->bindParam(":lastname", $lastname);
+            $statement->bindParam(":email", $email);
+            $statement->bindParam(":password", $password);
+            $statement->bindParam(":token", $token);
+            $statement->execute();
+
+            //Autenticar usuário caso auth seja true
+            if($authUser){
+                $this->setTokenToSession($user->getToken());
+            }
+
         }
 
         public function update(User $user){
-            //TOOO
+            //TODO:
         }
 
         public function verifyByToken($protected = false){
-            //TOOO
+            //TODO:
         }
 
         public function setTokenToSession($token, $redirect = true){
-            //TOOO
+            //Salvar token na session
+            $_SESSION["token"] = $token;
+
+            if($redirect){
+                //Redireciona para o perfil do usuário
+                $this->message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+            }
         }
 
         public function authenticateUser($email, $password){
-            //TOOO
+            //TODO:
         }
 
         public function findByEmail($email){
@@ -56,15 +85,15 @@
         }
 
         public function findById($id){
-            //TOOO
+            //TODO:
         }
 
         public function findByToken($token){
-            //TOOO
+            //TODO:
         }
 
         public function changePassword(User $user){
-            //TOOO
+            //TODO:
         }
     }
 
